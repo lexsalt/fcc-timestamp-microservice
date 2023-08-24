362ed99a91338a5d9d23ca473cab0beadf8ccc04
emilianosal <emilianosal@hotmail.com>
2023-08-24 15:03:17 +0200

    Upload to github

--- index.js
+++ index.js
@@ -14,28 +14,22 @@ app.use(function middleware(req, res, next){
   })
 
 app.get('/', (req, res) => {
-    //console.log(req).length
-
     res.sendFile(__dirname + "/view/index.html");
 })
 
 app.get('/api/:date', (req,res) => {
     let data = req.params.date
-    console.log(data)
-    console.log(data.length)
-    console.log(typeof(data))
     function check(data) {
-        console.log("ama")
         let date = new Date(data)
         let dateNumber = new Date(Number(data))
         if (date.getTime() === date.getTime()) {
-            console.log("date from string")
+            //console.log("date from string")
             return {
                 unix: date.getTime(),
                 utc: date.toGMTString()
             }
         } else if (date.getTime() !== date.getTime() & dateNumber.getTime() === dateNumber.getTime()) {
-            console.log("date from number")
+            //console.log("date from number")
             return {
                 unix: new Date(dateNumber).getTime(),
                 utc: dateNumber.toGMTString()
@@ -51,13 +45,11 @@ app.get('/api/:date', (req,res) => {
 })
 app.get('/api/', (req,res) => {
     console.log("1")
-    res.json( { error : "Invalid Date" } )
+    const unixTime = Date.now();
+    let dateTime = new Date(unixTime)
+    let date = dateTime.toGMTString()
+    res.json( { unix: unixTime, utc: date } )
 })
-app.get('/api', (req,res) => {
-    console.log("2")
-    res.json( { error : "Invalid Date" } )
-})
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
